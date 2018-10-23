da6ac151530c19b7fa76728fe28efca3d97c9228
Rinni Swift <rinradaswift@gmail.com>
2018-10-23 13:12:52 -0700

    web loads movie reviews from the root route

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const app = express()
 
+// mock arrays of projects
+let reviews = [
+	{ title: "Great Review", movieTitle: "Batman II"},
+	{ title: "Awesome Movie", movieTitle: "Titanic"}
+]
+
+
 // set the express handle bars
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+
+
 app.get('/', (req,res) => {
 	// extend root route to render home.handlebars
-	res.render('home', { msg: 'handlebars are cool!' });
+	res.render('reviews-index', { reviews: reviews });
 })
 
 app.listen(3000, () => {
