93e47c9dcc42264cac79ed8b2827d060dbfae692
Rinni Swift <rinradaswift@gmail.com>
2018-10-23 16:50:54 -0700

    implemented mongodb

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+
+const Review = mongoose.model('Review', {
+  title: String,
+  movieTitle: String
+});
+
 
 // mock arrays of projects
-let reviews = [
-	{ title: "Great Review", movieTitle: "Batman II"},
-	{ title: "Awesome Movie", movieTitle: "Titanic"}
-]
+// let reviews = [
+// 	{ title: "Great Review", movieTitle: "Batman II"},
+// 	{ title: "Awesome Movie", movieTitle: "Titanic"}
+// ]
 
 
 // set the express handle bars
@@ -15,10 +23,23 @@ app.set('view engine', 'handlebars');
 
 
 
-app.get('/', (req,res) => {
-	// extend root route to render home.handlebars
-	res.render('reviews-index', { reviews: reviews });
+app.get('/', (req, res) => {
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
+
+Review.find()
+  .then(review => {
+  // Code in here is executed when the promise resolves
+  })
+  .catch(err => {
+
+  });
 
 app.listen(3000, () => {
 	console.log('App listening on port 3000!')
